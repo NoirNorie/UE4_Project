b22c7d86430c584dd9ed008bd80434181c3dabcd
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2021-12-25 17:16:33 +0900

    Add files via upload
    캐릭터 작업
    - 디폴트 캐릭터에서 언리얼 마켓에서 제공하는 캐릭터로 변경함

---
+++ MeshMerger.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MeshMergeFunctionLibrary.h"
+#include "MeshMerger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PP_API UMeshMerger : public UMeshMergeFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
