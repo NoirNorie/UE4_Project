501c0b13971a6ef2a52ff22ec70065626e6bd213
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-01-21 02:30:18 +0900

    Delete VCustomUI.cpp

--- VCustomUI.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "VCustomUI.h"
-
-UVCustomUI::UVCustomUI(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-
-}
